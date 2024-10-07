1349415593e517613d7181f06eb4b773b5d7f932
Nick <nmalyarchuk07@gmail.com>
2024-10-07 13:59:49 -0400

    arrays

---
+++ index.js
@@ -0,0 +1,16 @@
+//1
+const students = [
+  { name: "Alice", age: 20, grade: 85 },
+  { name: "Bob", age: 22, grade: 90 },
+  { name: "Charlie", age: 21, grade: 78 },
+  { name: "Diana", age: 23, grade: 95 },
+];
+// 1. Log each student’s name and grade
+students.forEach((el) => console.log(el.name, el.grade));
+
+//2
+
+const topStudents = students
+  .filter((el) => el.grade > 80)
+
+  .filter((topStudents) => console.log(topStudents.name, topStudents.grade));
